850606a0c8caa1024beaf06c9154ab8c31620338
Dominic Chang <dchang3419@hotmail.com>
2023-07-30 14:39:21 -0400

    Reproduced Brio-Wu shock tube
    A description can be found here 
    
    https://www.astro.princeton.edu/~jstone/Athena/tests/brio-wu/Brio-Wu.html

--- MHD.jl
+++ MHD.jl
@@ -5,18 +5,18 @@ using LinearAlgebra
 #######################################################################################
 # Equation Declaration and Definitions 
 #######################################################################################
-struct mhd_equation <: Trixi.AbstractEquations{1, 8 }end
-Trixi.varnames(::typeof(Trixi.cons2cons), ::mhd_equation) = ("ρ", "ρvx", "ρvy", "ρvz", "Bx", "By", "Bz", "E")
-Trixi.varnames(::typeof(Trixi.cons2prim), ::mhd_equation) = ("ρ", "vx", "vy", "vz", "Bx", "By", "Bz", "p")
+struct MHD_Equation <: Trixi.AbstractEquations{1, 8 }end
+Trixi.varnames(::typeof(Trixi.cons2cons), ::MHD_Equation) = ("ρ", "ρvx", "ρvy", "ρvz", "Bx", "By", "Bz", "E")
+Trixi.varnames(::typeof(Trixi.cons2prim), ::MHD_Equation) = ("ρ", "vx", "vy", "vz", "Bx", "By", "Bz", "p")
 
-function Trixi.prim2cons(prim, equation::mhd_equation)
+function Trixi.prim2cons(prim, equation::MHD_Equation)
     ρ, vx, vy, vz, Bx, By, Bz, p = prim
     γ = 2.0
-    E = (p/(γ-1) + 1/2*(Bx^2+By^2+Bz^2) + ρ/2*(vx^2+vy^2+vz^2))
+    E = p/(γ-1) + (*(Bx^2+By^2+Bz^2) + ρ*(vx^2+vy^2+vz^2))/2
     return SVector(ρ, ρ*vx, ρ*vy, ρ*vz, Bx, By, Bz, E)
 end
 
-function Trixi.cons2prim(u, equation::mhd_equation)
+function Trixi.cons2prim(u, equation::MHD_Equation)
     ρ, ρvx, ρvy, ρvz, Bx, By, Bz, E = u
 
     vx = ρvx/ρ
@@ -28,7 +28,7 @@ function Trixi.cons2prim(u, equation::mhd_equation)
   return SVector(ρ, vx, vy, vz, Bx, By, Bz, p)
 end
 
-function density_pressure(u, equations::mhd_equation)
+function density_pressure(u, equations::MHD_Equation)
   ρ, vx, vy, vz, Bx, By, Bz, E = u
   γ = 2.0
   p = (γ-1.0)*(E - ρ/2*(vx^2+vy^2+vz^2) - 1/2*(Bx^2+By^2+Bz^2))
@@ -36,7 +36,7 @@ function density_pressure(u, equations::mhd_equation)
   return rho_times_p
  end
 
-Trixi.flux(u, n, equation::mhd_equation) = begin
+Trixi.flux(u, n, equation::MHD_Equation) = begin
   ρ, ρvx, ρvy, ρvz, Bx, By, Bz, E = u
 
   γ = 2.0
@@ -45,7 +45,7 @@ Trixi.flux(u, n, equation::mhd_equation) = begin
   vz = ρvz/ρ
 
   p = (γ-1.0)*(E - ρ/2*(vx^2+vy^2+vz^2) - 1/2*(Bx^2+By^2+Bz^2))
-  ps = p + 1/2*(Bx^2+By^2+Bz^2)
+  ps = p + (Bx^2+By^2+Bz^2)/2
   return SVector(
     ρ*vx,
     ρ*vx^2 + ps -Bx^2,
@@ -57,12 +57,58 @@ Trixi.flux(u, n, equation::mhd_equation) = begin
     (E+ps)*vx - Bx*(Bx*vx+By*vy+Bz*vz)
   )
 end
-function Trixi.max_abs_speed_naive(u_ll, u_rr, orientation, equation::Main.MHD.mhd_equation)
+
+Trixi.flux_godunov(u_ll, u_rr, n, equation::MHD_Equation) = begin
+  ρ_ll, ρvx_ll, ρvy_ll, ρvz_ll, Bx_ll, By_ll, Bz_ll, E_ll = u_ll
+  ρ_rr, ρvx_rr, ρvy_rr, ρvz_rr, Bx_rr, By_rr, Bz_rr, E_rr = u_rr
+
+  γ = 2.0
+  vx_ll = ρvx_ll/ρ_ll
+  vy_ll = ρvy_ll/ρ_ll
+  vz_ll = ρvz_ll/ρ_ll
+
+  p_ll = (γ-1.0)*(E_ll - ρ_ll/2*(vx_ll^2+vy_ll^2+vz_ll^2) - 1/2*(Bx_ll^2+By_ll^2+Bz_ll^2))
+  ps_ll = p_ll + 1/2*(Bx_ll^2+By_ll^2+Bz_ll^2)
+
+  f_ll = (
+    ρ_ll*vx_ll,
+    ρ_ll*vx_ll^2 + ps_ll -Bx_ll^2,
+    ρ_ll*vx_ll*vy_ll - Bx_ll*By_ll,
+    ρ_ll*vx_ll*vz_ll - Bx_ll*Bz_ll,
+    0,
+    By_ll*vx_ll - Bx_ll*vy_ll,
+    Bz_ll*vx_ll - Bx_ll*vz_ll,
+    (E_ll+ps_ll)*vx_ll - Bx_ll*(Bx_ll*vx_ll+By_ll*vy_ll+Bz_ll*vz_ll)
+  )
+
+  vx_rr = ρvx_rr/ρ_rr
+  vy_rr = ρvy_rr/ρ_rr
+  vz_rr = ρvz_rr/ρ_rr
+
+  p_rr = (γ-1.0)*(E_rr - ρ_rr/2*(vx_rr^2+vy_rr^2+vz_rr^2) - 1/2*(Bx_rr^2+By_rr^2+Bz_rr^2))
+  ps_rr = p_rr + 1/2*(Bx_rr^2+By_rr^2+Bz_rr^2)
+
+  f_rr = (
+    ρ_rr*vx_rr,
+    ρ_rr*vx_rr^2 + ps_rr -Bx_rr^2,
+    ρ_rr*vx_rr*vy_rr - Bx_rr*By_rr,
+    ρ_rr*vx_rr*vz_rr - Bx_rr*Bz_rr,
+    0,
+    By_rr*vx_rr - Bx_rr*vy_rr,
+    Bz_rr*vx_rr - Bx_rr*vz_rr,
+    (E_rr+ps_rr)*vx_rr - Bx_rr*(Bx_rr*vx_rr+By_rr*vy_rr+Bz_rr*vz_rr)
+  )
+
+
+  return f_ll .+ f_rr
+end
+
+function Trixi.max_abs_speed_naive(u_ll, u_rr, orientation, equation::Main.MHD.MHD_Equation)
   _, vx_ll, vy_ll, vz_ll, _, _, _, _ = u_ll
   _, vx_rr, vy_rr, vz_rr, _, _, _, _ = u_rr
 
-  v_ll = max(abs(vx_ll), abs(vy_ll))
-  v_rr = max(abs(vx_rr), abs(vy_rr))
+  v_ll = vx_ll
+  v_rr = vx_rr
 
   return 2*max(abs(v_ll), abs(v_rr)) 
 
--- MHDelixir.jl
+++ MHDelixir.jl
@@ -2,21 +2,20 @@ include("./MHD.jl")
 using Trixi
 using OrdinaryDiffEq
 using Plots
-equation = MHD.mhd_equation()
+equation = MHD.MHD_Equation()
 
-function initial_condition(x, t, equation::MHD.mhd_equation)
+function initial_condition(x, t, equation::MHD.MHD_Equation)
   if x[1] < 0.0
-                            #ρ, vx, vy, vz, Bx,  By, Bz, p = prim
-    return Trixi.prim2cons(( 1, 0,  0,  0,  0.75,1,  0,  1), equation)
+    return Trixi.prim2cons((1,0,0,0,.75,1,0,1), equation)
   else
-    return Trixi.prim2cons((0.125,0,0,0,0.75,-1,0,0.1), equation)
+    return Trixi.prim2cons((0.125,0,0,0,.75,-1,0,0.1), equation)
   end
 end
 
-function boundary_in(x, t, equation::MHD.mhd_equation)
+function boundary_in(x, t, equation::MHD.MHD_Equation)
     return Trixi.prim2cons((1,0,0,0,0.75,1,0,1), equation)
 end
-function boundary_out(x, t, equation::MHD.mhd_equation)
+function boundary_out(x, t, equation::MHD.MHD_Equation)
     return Trixi.prim2cons((0.125,0,0,0,0.75,-1,0,0.1), equation)
 end
 boundary_conditions = (x_neg=Trixi.BoundaryConditionDirichlet(boundary_in),
@@ -25,7 +24,7 @@ boundary_conditions = (x_neg=Trixi.BoundaryConditionDirichlet(boundary_in),
 surface_flux = flux_lax_friedrichs
 volume_flux  = flux#_shima_etal
 
-basis = Trixi.LobattoLegendreBasis(1)
+basis = Trixi.LobattoLegendreBasis(0)
 indicator_sc = IndicatorHennemannGassner(equation, basis,
                                          alpha_max=0.5,
                                          alpha_min=0.001,
@@ -39,8 +38,8 @@ volume_integral = Trixi.VolumeIntegralShockCapturingHG(indicator_sc;
 solver = Trixi.DGSEM(basis, surface_flux)#, volume_integral)
 
 mesh = Trixi.TreeMesh((-1.0,), (1.0,),
-                initial_refinement_level=8,
-                n_cells_max=10^5, periodicity=false)
+                initial_refinement_level=13,
+                n_cells_max=20_000, periodicity=false)
 
 
 semi = Trixi.SemidiscretizationHyperbolic(mesh, equation, initial_condition, solver, boundary_conditions=boundary_conditions)#, source_terms=mhd.source_terms)
@@ -53,7 +52,8 @@ ode = Trixi.semidiscretize(semi, tspan)
 #callbacks = CallbackSet(summary_callback)
 
 # OrdinaryDiffEq's `solve` method evolves the solution in time and executes the passed callbacks
-sol = solve(ode, OrdinaryDiffEq.RDPK3SpFSAL49(), save_everystep=true)#, maxiters=1e5, reltol=1e-8, abstol=1e-85; dt=0.01);
+#sol = solve(ode, OrdinaryDiffEq.RDPK3SpFSAL49(), save_everystep=true)#, maxiters=1e5, reltol=1e-8, abstol=1e-85; dt=0.01);
+sol = solve(ode, OrdinaryDiffEq.RK4(),dt=(0.2/10_000))#, maxiters=1e5, reltol=1e-8, abstol=1e-85; dt=0.01);
 
 # Print the timer summary
 #summary_callback()
@@ -62,9 +62,9 @@ sol = solve(ode, OrdinaryDiffEq.RDPK3SpFSAL49(), save_everystep=true)#, maxiters
 # A new setup with dissipation
 pd = Trixi.PlotData1D(sol; solution_variables=Trixi.cons2prim)
 time = sol.t
-anim = @animate for (i,t) in enumerate(time)
-  plot(sol.u[i][1:8:end]; solution_variables=Trixi.cons2cons, title = "$(t)")
-end
-gif(anim, (@__DIR__)*"/MHD.gif", fps = 8)
+#anim = @animate for (i,t) in enumerate(time)
+#  plot(sol.u[i][2:8:end]; solution_variables=Trixi.cons2prim, title = "$(t)")
+#end
+#gif(anim, (@__DIR__)*"/MHD.gif", fps = 8)
 
 plot(sol; solution_variables=Trixi.cons2prim)
